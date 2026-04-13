5bae5c5ea1b2847f6fd9d2c0f8a02d4885163d50
hadygust <hadygust2883@gmail.com>
2026-04-13 17:46:48 +0700

    completed basic http server structure with gin

--- cmd/app.go
+++ cmd/app.go
@@ -1,1 +1,38 @@
-package cmd
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx"
+)
+
+// mount
+func (app *application) mount() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.Data(http.StatusOK, "application/json", []byte("Hello World"))
+	})
+
+	return r
+}
+
+// run
+func (app *application) run(h *gin.Engine) {
+	h.Run(app.config.address)
+}
+
+type application struct {
+	config config
+	db     pgx.Conn
+}
+
+type config struct {
+	address string
+	db      dbConfig
+}
+
+type dbConfig struct {
+	dsn string
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,23 @@
-package cmd
+package main
+
+import (
+	"github.com/hadygust/movie/internal/env"
+	"github.com/jackc/pgx"
+)
 
 func main() {
 
+	cfg := config{
+		address: ":8080",
+		db: dbConfig{
+			dsn: env.GetString("dsn", ""),
+		},
+	}
+
+	app := application{
+		config: cfg,
+		db:     pgx.Conn{},
+	}
+
+	app.run(app.mount())
 }
